e771548a114cf87f24fd887d7fc76ebbbd1bcd55
Muhammed Abdelmukaram <muhammed.abdelmukaram@stu.ssst.edu.ba>
2024-03-26 22:28:39 +0100

    Tried to add icon fav

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,6 +11,7 @@ class MyDocument extends Document {
                         href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap"
                         rel="stylesheet"
                     />
+                    <link rel="icon" href="/icon.ico" />
                     {/* Any other head content */}
                 </Head>
                 <body>
