4aadd8156c3fbd1cb787afe4e9ed01c4fc87bf43
Elmar Ismayilov <bamazy@gmail.com>
2024-03-28 23:02:45 +0800

    fix signin

--- src/app/api/signin/route.js
+++ src/app/api/signin/route.js
@@ -1,51 +1,45 @@
-// pages/api/login.js
 import { NextResponse } from "next/server";
-import dbConnect from "@/lib/dbConnect";
-import UserProfile from "@/models/userModel";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-export default async function handler(req, res) {
-    await dbConnect();
+import dbConnect from "@/lib/dbConnect";
+import UserProfile from "@/models/userModel";
 
-    if (req.method === 'POST') {
-        try {
-            const { email, password } = req.body;
+export async function POST(request) {
+  await dbConnect();
 
-            // Find the user by email
-            const user = await UserProfile.findOne({ email });
+  try {
+    const data = await request.json();
+    const { email, password } = data;
 
-            if (!user) {
-                return new NextResponse(JSON.stringify({ error: 'User not found' }), { status: 404 });
-            }
+    const user = await UserProfile.findOne({ email });
 
-            // Verify the password
-            const isMatch = await bcrypt.compare(password, user.password);
-
-            if (!isMatch) {
-                return new NextResponse(JSON.stringify({ error: 'Invalid credentials' }), { status: 401 });
-            }
-
-            // User authenticated, create a token
-            const token = jwt.sign(
-                { userId: user._id },
-                process.env.JWT_SECRET, // Ensure this is set
-                { expiresIn: '1h' }
-            );
-
-            // Return the token to the client
-            const response = new NextResponse(JSON.stringify({ token }), {
-                status: 200,
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
-            return response;
-        } catch (error) {
-            return new NextResponse(JSON.stringify({ error: 'Internal server error' }), { status: 500 });
-        }
-    } else {
-        res.setHeader('Allow', ['POST']);
-        return new NextResponse(`Method ${req.method} Not Allowed`, { status: 405 });
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
     }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return NextResponse.json(
+        { error: "Invalid credentials" },
+        { status: 401 }
+      );
+    }
+
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET, // Ensure this is set
+      { expiresIn: "1h" }
+    );
+
+    return NextResponse.json({ token });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: error.message,
+      },
+      { status: 500 }
+    );
+  }
 }
